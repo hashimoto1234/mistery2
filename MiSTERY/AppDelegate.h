9e79e1f4c9206034c0484ec050b16b35686466e8
橋本直樹 <hashimotonaoki@hashimotonaoki-no-macbook-pro.local>
2016-11-26 22:03:06 +0900

    Initial Commit

---
+++ MiSTERY/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MiSTERY
+//
+//  Created by 橋本直樹 on 2016/11/26.
+//  Copyright © 2016年 橋本直樹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
