10f3791b9833c42281be0c625d9fad62ccc7bb4a
naokihashimoto <h.naoki.k@gmail.com>
2016-12-16 19:12:19 +0900

    aaaaaaa

---
+++ MiSTERY/Model/keyWordDefinition.h
@@ -0,0 +1,13 @@
+//
+//  keyWordDefinition.h
+//  MiSTERY
+//
+//  Created by 橋本直樹 on 2016/12/16.
+//  Copyright © 2016年 橋本直樹. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface keyWordDefinition : NSObject
+
+@end
---
+++ MiSTERY/Model/keyWordDefinition.m
@@ -0,0 +1,13 @@
+//
+//  keyWordDefinition.m
+//  MiSTERY
+//
+//  Created by 橋本直樹 on 2016/12/16.
+//  Copyright © 2016年 橋本直樹. All rights reserved.
+//
+
+#import "keyWordDefinition.h"
+
+@implementation keyWordDefinition
+
+@end
